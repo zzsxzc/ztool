8305b007fa6f05058c36336db387555d5a73dcb7
zhengzhaosheng <1350111595@qq.com>
2020-10-21 22:26:48 +0800

    1、junit单元测试:插件加pom依赖引入;2、测试泛型，<?>通配符号未详细测试

---
+++ src/com/smallMorning/test/CodeProducerTest.java
@@ -0,0 +1,13 @@
+package com.smallMorning.test;
+
+public class CodeProducerTest {
+
+    @org.junit.Test
+    public void codeMakerWordAndNumber() {
+        System.out.println("测试跑起来了？");
+    }
+
+    @org.junit.Test
+    public void listOfCode() {
+    }
+}
