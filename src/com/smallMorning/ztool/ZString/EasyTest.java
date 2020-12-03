7ba68abe61674604ec0d60d85c897d5203399a4e
zzsxzc <1350111595@qq.com>
2020-12-03 09:40:03 +0800

    把一段文本放在双引号里面，idea可以自动转义

--- src/com/smallMorning/ztool/ZString/EasyTest.java
+++ src/com/smallMorning/ztool/ZString/EasyTest.java
@@ -15,6 +15,10 @@ public class EasyTest {
         System.out.println(str2);
         System.out.println("\\");
         System.out.println("\"");
+
+        //idea可以自动转义
+        //String transport = "{\"Creator\":\"\",\"NeedUpDateFields\":[],\"NeedReturnFields\":[],\"IsDeleteEntry\":\"true\",\"SubSystemId\":\"\",\"IsVerifyBaseDataField\":\"false\",\"IsEntryBatchFill\":\"true\",\"ValidateFlag\":\"true\",\"NumberSearch\":\"true\",\"InterationFlags\":\"\",\"Model\":{\"FID\":0,\"FName\":\"\",\"FStaffNumber\":\"\",\"FMobile\":\"\",\"FTel\":\"\",\"FEmail\":\"\",\"FDescription\":\"\",\"FAddress\":\"\",\"FUseOrgId\":{\"FNumber\":\"\"},\"FCreateOrgId\":{\"FNumber\":\"\"},\"FBranchID\":{\"FNUMBER\":\"\"},\"FCreateSaler\":\"false\",\"FCreateUser\":\"false\",\"FCreateCashier\":\"false\",\"FCashierGrp\":{\"FNUMBER\":\"\"},\"FSalerId\":{\"FNUMBER\":\"\"},\"FCashierId\":{\"FNUMBER\":\"\"},\"FUserId\":{\"FUSERACCOUNT\":\"\"},\"FPostId\":{\"FNUMBER\":\"\"},\"FJoinDate\":\"1900-01-01\",\"FUniportalNo\":\"\",\"FSHRMapEntity\":{\"FMAPID\":0},\"FPostEntity\":[{\"FENTRYID\":0,\"FWorkOrgId\":{\"FNumber\":\"\"},\"FPostDept\":{\"FNumber\":\"\"},\"FPost\":{\"FNumber\":\"\"},\"FStaffStartDate\":\"1900-01-01\",\"FIsFirstPost\":\"false\"}],\"FBankInfo\":[{\"FBankId\":0,\"FBankCountry\":{\"FNumber\":\"\"},\"FBankCode\":\"\",\"FBankTypeRec\":{\"FNUMBER\":\"\"},\"FBankHolder\":\"\",\"FTextBankDetail\":\"\",\"FBankDetail\":{\"FNUMBER\":\"\"},\"FOpenBankName\":\"\",\"FOpenAddressRec\":\"\",\"FCNAPS\":\"\",\"FBankCurrencyId\":{\"FNUMBER\":\"\"},\"FBankIsDefault\":\"false\",\"FBankDesc\":\"\"}]}}";
+
     }
 
 }
