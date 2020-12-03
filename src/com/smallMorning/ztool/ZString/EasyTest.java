a2d570f8c13e3e600b9b20d45dc120bec8f8fe64
zzsxzc <1350111595@qq.com>
2020-12-03 09:18:28 +0800

    json转义ok

--- src/com/smallMorning/ztool/ZString/EasyTest.java
+++ src/com/smallMorning/ztool/ZString/EasyTest.java
@@ -1,15 +1,20 @@
 package com.smallMorning.ztool.ZString;
 
+import com.alibaba.druid.sql.visitor.ExportParameterizedOutputVisitor;
+
 public class EasyTest {
 
-    public static void mian(String[] args){
+    public static void main(String[] args){
         String str1 = "as\"dfg";
+        //String str1 = "as\"dfg";
         String str2 = null;
-        String[] tihuan = {"\""};
-        String daiti = "\\\"";
+        String[] tihuan = {"\""};//表示"
+        String daiti = "\\\\";//表示的字符串是\
         str2 = ZString.zReplace(str1,tihuan,daiti);
         System.out.println(str1);
         System.out.println(str2);
+        System.out.println("\\");
+        System.out.println("\"");
     }
 
 }
--- src/com/smallMorning/ztool/ZString/ZString.java
+++ src/com/smallMorning/ztool/ZString/ZString.java
@@ -34,20 +34,19 @@ public class ZString {
             }
         }
 
-        String strToChange = sbf.toString().trim();
-        System.out.println(strToChange);
+        String str2 = sbf.toString().trim();
 
-        String str2 = null;
-        System.out.println(sbf);
         String[] toChange = {"\""};
-        str2 = ZString.zReplace(sbf.toString(),toChange,"\\\"");
-        System.out.println(str2);
-
-        String str3 = "\\\"\\\"NeedUpDateFields\\\":[],\\\"NeedReturnFields\\\":[],\\\"IsDeleteEntry\\\":\\\"true\\\", \\\"SubSystemId\\\":\\\"\\\",\" +\n" + "\"\\\"IsVerifyBaseDataField\\\":\\\"false\\\", \\\"IsEntryBatchFill\\\":\\\"true\\\", \\\"ValidateFlag\\\":\\\"true\\\", \\\"NumberSearch\\\":\\\"true\\\", \\\"InterationFlags\\\":\\\"\\\",\" +\n" + "\" \\\"Model\\\":{ \\\"FID\\\":0,\\\"FName\\\":\\\"\\\", \\\"FStaffNumber\\\":\\\"\\\", \\\"FMobile\\\":\\\"\\\", \\\"FTel\\\":\\\"\\\", \\\"FEmail\\\":\\\"\\\", \\\"FDescription\\\":\\\"\\\", \\\"FAddress\\\":\\\"\\\", \" +\n" + "\"\\\"FUseOrgId\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FCreateOrgId\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FBranchID\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FCreateSaler\\\":\\\"false\\\", \" +\n" + "\"\\\"FCreateUser\\\":\\\"false\\\", \\\"FCreateCashier\\\":\\\"false\\\", \\\"FCashierGrp\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FSalerId\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\" +\n" + "\"\\\"FCashierId\\\":{ \\\"FNUMBER\\\":\\\"\\\"\\\"},\\\"FUserId\\\":{ \\\"FUSERACCOUNT\\\":\\\"\\\"},\\\"FPostId\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FJoinDate\\\":\\\"1900 - 01 - 01\\\", \" +\n" + "\"\\\"FUniportalNo\\\":\\\"\\\", \\\"FSHRMapEntity\\\":{ \\\"FMAPID\\\":0},\\\"FPostEntity\\\":[{\\\"FENTRYID\\\":0,\\\"FWorkOrgId\\\":{ \\\"FNumber\\\":\\\"\\\"},\" +\n" + "\"\\\"FPostDept\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FPost\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"\\\"FStaffStartDate\\\":\\\"1900 - 01 - 01\\\", \\\"FIsFirstPost\\\":\\\"false\\\"}],\" +\n" + "\"\\\"FBankInfo\\\":[{\\\"FBankId\\\":0,\\\"FBankCountry\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FBankCode\\\":\\\"\\\", \\\"FBankTypeRec\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\" + \n" + "\"\\\"FBankHolder\\\":\\\"\\\", \\\"FTextBankDetail\\\":\\\"\\\", \\\"FBankDetail\\\":{ \\\"FNUMBER\\\":\\\"\\\"\\\"},\\\"FOpenBankName\\\":\\\"\\\"\\\", \"+\n" + "\"\\\"FOpenAddressRec\\\":\\\"\\\", \\\"FCNAPS\\\":\\\"\\\", \\\"FBankCurrencyId\\\":{ \\\"FNUMBER\\\":\\\"\\\"\\\"},\\\"FBankIsDefault\\\":\\\"false\\\", \\\"FBankDesc\\\":\\\"\\\"}]}}\\\"";
-        String str4 = str3.trim();
-        System.out.println(strToChange.length());
-        System.out.println(str4.length());
-        System.out.println(str2.equals(str4));
+        String strResult = null;
+        strResult = ZString.zReplace(str2,toChange,"\\\\\"");
+        String str3 = "{\\\"Creator\\\":\\\"\\\",\\\"NeedUpDateFields\\\":[],\\\"NeedReturnFields\\\":[],\\\"IsDeleteEntry\\\":\\\"true\\\", \\\"SubSystemId\\\":\\\"\\\",\\\"IsVerifyBaseDataField\\\":\\\"false\\\", \\\"IsEntryBatchFill\\\":\\\"true\\\", \\\"ValidateFlag\\\":\\\"true\\\", \\\"NumberSearch\\\":\\\"true\\\", \\\"InterationFlags\\\":\\\"\\\",\\\"Model\\\":{ \\\"FID\\\":0,\\\"FName\\\":\\\"\\\", \\\"FStaffNumber\\\":\\\"\\\", \\\"FMobile\\\":\\\"\\\", \\\"FTel\\\":\\\"\\\", \\\"FEmail\\\":\\\"\\\", \\\"FDescription\\\":\\\"\\\", \\\"FAddress\\\":\\\"\\\",\\\"FUseOrgId\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FCreateOrgId\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FBranchID\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FCreateSaler\\\":\\\"false\\\",\\\"FCreateUser\\\":\\\"false\\\", \\\"FCreateCashier\\\":\\\"false\\\", \\\"FCashierGrp\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FSalerId\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FCashierId\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FUserId\\\":{ \\\"FUSERACCOUNT\\\":\\\"\\\"},\\\"FPostId\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FJoinDate\\\":\\\"1900 - 01 - 01\\\", \\\"FUniportalNo\\\":\\\"\\\", \\\"FSHRMapEntity\\\":{ \\\"FMAPID\\\":0},\\\"FPostEntity\\\":[{\\\"FENTRYID\\\":0,\\\"FWorkOrgId\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FPostDept\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FPost\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FStaffStartDate\\\":\\\"1900 - 01 - 01\\\", \\\"FIsFirstPost\\\":\\\"false\\\"}],\\\"FBankInfo\\\":[{\\\"FBankId\\\":0,\\\"FBankCountry\\\":{ \\\"FNumber\\\":\\\"\\\"},\\\"FBankCode\\\":\\\"\\\", \\\"FBankTypeRec\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FBankHolder\\\":\\\"\\\", \\\"FTextBankDetail\\\":\\\"\\\", \\\"FBankDetail\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FOpenBankName\\\":\\\"\\\",\\\"FOpenAddressRec\\\":\\\"\\\", \\\"FCNAPS\\\":\\\"\\\", \\\"FBankCurrencyId\\\":{ \\\"FNUMBER\\\":\\\"\\\"},\\\"FBankIsDefault\\\":\\\"false\\\", \\\"FBankDesc\\\":\\\"\\\"}]}}";
+        System.out.println("str3 length: "+str3.length());
+        String str4 = str3.replaceAll(" ","");
+        System.out.println("strResult length: "+strResult.length());
+        System.out.println("str4 length: "+str4.length());
+        System.out.println(strResult);
+        System.out.println(str4);
+        System.out.println(strResult.equals(str4));
     }
 
     //转义
