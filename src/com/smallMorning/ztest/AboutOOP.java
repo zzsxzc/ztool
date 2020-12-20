f5c632ae31793400b0ae7a7cf4aa48621e2e1418
zhengzhaosheng <1350111595@qq.com>
2020-12-20 14:42:15 +0800

    aboutOOP

---
+++ src/com/smallMorning/ztest/AboutOOP.java
@@ -0,0 +1,10 @@
+package com.smallMorning.ztest;
+/**
+*@Author zheng zs
+*@Date 14:36 2020/12/20
+*@Description 关于面向对象 什么叫做面向对象？
+ * 面向对象的核心是类和对象，类是具体事务的抽象，比如把猫和狗都抽象化为动物，对象是类的实例化，
+ * 封装、继承、多态
+**/
+public class AboutOOP {
+}
