9c22cf3bfc70a8ec62bc85a3c03c75d4d442b05b
zzsxzc <1350111595@qq.com>
2020-12-25 13:34:06 +0800

    测试泛型方法

--- src/com/smallMorning/ztest/TestGenetic1.java
+++ src/com/smallMorning/ztest/TestGenetic1.java
@@ -9,14 +9,27 @@ import java.util.List;
 *@Description 泛型测试1
 **/
 public class TestGenetic1 {
+
+    private static <T extends Number> void add(T a ,T b){
+        //用泛型实现多种数据类型的假发貌似并没有省多少代码
+        if(a.getClass()==Integer.class&&b.getClass()==Integer.class){
+            System.out.println("Integer     "+a+"+"+b+"="+(a.intValue()+b.intValue()));
+        }else if(a.getClass()==Double.class&&b.getClass()==Double.class){
+            System.out.println("Double     "+a+"+"+b+"="+(a.doubleValue()+b.doubleValue()));
+        }else{
+            System.out.println("不提供计算！");
+        }
+    }
     public static void main(String args[]){
-        List<b> list= new ArrayList<>(); //正确写法
+        /*List<b> list= new ArrayList<>(); //正确写法
         //List<> list2= new ArrayList<b>(); //这种写法会报错 受检异常 idea直接报错
         //List<b> list3= new ArrayList<b>(); //可以但没必要
         a A = new a();
         b B = new b();
         c C = new c();
-        list.add(B);//没使用通配符的话只能使用确定的类名来实例化
+        list.add(B);//没使用通配符的话只能使用确定的类名来实例化*/
+        add(1,2);
+        add(1,2.2);
     }
 }
 class a{
